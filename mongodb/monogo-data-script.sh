7d0abc124ed960cd881a9ee5510b356ec165bad3
Jason Carvalho <jase@jasoncarvalho.com>
2023-04-14 10:59:18 +0100

    Debugging

--- mongodb/monogo-data-script.sh
+++ mongodb/monogo-data-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "*** INSTALLING MONGODB POLICIES DATA ***"
 mongoimport --db=spice --collection=spice_policies --file=/home/policies_array.json --jsonArray 
+echo "*** POLICIES DATA IMPORTED ***"
